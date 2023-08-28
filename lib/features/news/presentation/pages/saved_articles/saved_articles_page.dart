f0b49986d0739b5a127383b70f7ffacff7f65fdd
Cromuel Oliver <cromuelbarut@gmail.com>
2023-08-28 15:48:31 +0800

    created scenario handler for when user removed article and for error

--- lib/features/news/presentation/pages/saved_articles/saved_articles_page.dart
+++ lib/features/news/presentation/pages/saved_articles/saved_articles_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_clean_architecture/core/utils/helpers.dart';
 
 import 'package:news_app_clean_architecture/features/news/presentation/bloc/bloc.dart';
 import 'package:news_app_clean_architecture/features/news/presentation/pages/saved_articles/components/saved_articles_view.dart';
@@ -20,9 +21,29 @@ class SavedArticlesPage extends StatefulWidget {
 }
 
 class _SavedArticlesPageState extends State<SavedArticlesPage> {
+  void _localArticlesListener(BuildContext ctx, LocalArticlesState state) {
+    final bool removing = state.actionType == LocalArticlesActionType.removing;
+
+    if (state.status == LocalArticlesStatus.success && removing) {
+      showSnackBar(ctx, message: 'Article removed.');
+    }
+
+    if (state.status == LocalArticlesStatus.failure && removing) {
+      showSnackBar(ctx, message: 'Something went wrong.');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SavedArticlesView();
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<LocalArticlesBloc, LocalArticlesState>(
+          listenWhen: (prev, curr) => prev.status != curr.status,
+          listener: _localArticlesListener,
+        ),
+      ],
+      child: const SavedArticlesView(),
+    );
   }
 
   @override
